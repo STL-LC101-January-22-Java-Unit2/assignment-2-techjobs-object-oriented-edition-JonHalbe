a33cba22e1efc8713eedfc4bebd0ddb5cbeb9868
johnn <johnny.halbert@gmail.com>
2022-05-12 18:16:31 -0500

    Completed up to task 4 now

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -19,17 +19,7 @@ public class CoreCompetency {
         this();
         this.value = value;
     }
-    public String getValue() {
-        return value;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
     // Custom toString, equals, and hashCode methods:
 
     @Override
@@ -52,5 +42,15 @@ public class CoreCompetency {
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
+    public String getValue() {
+        return value;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
